4acee15501b18bfceb3f04265739896d15e2f652
Gary-D-Smith <76981226+Gary-D-Smith@users.noreply.github.com>
2023-11-30 09:12:23 -0500

    Update ImageSlider.js

--- ImageSlider.js
+++ ImageSlider.js
@@ -3,17 +3,12 @@ import { FlippablePhoto } from "./FlippablePhoto";
 import { Swiper, SwiperSlide } from 'swiper/react'
 import leftArrow from './assets/vectors/left-nav-arrow.svg'
 import rightArrow from './assets/vectors/right-nav-arrow.svg'
-// import { CardsEffectOptions } from "swiper/types";
-// import { CardsEffectOptions } from "swiper/types";
-// import { EffectCards } from "swiper";
 import 'swiper/css'
 import { Navigation, EffectCards } from 'swiper'
 import 'swiper/css/navigation'
 import 'swiper/css/effect-flip'
 import 'swiper/css/effect-cards'
 import 'swiper/css/keyboard'
-// import { SwiperNavButtons } from "./components/SwiperNavButton"
-
 
 export function ImageSlider({images}) {
     const navigationPrevRef = React.useRef(null)
@@ -56,56 +51,3 @@ export function ImageSlider({images}) {
         </div>
     );
 }
-
-
-    // images: [ id: theID, path: URL.createObjectURL(file), name: file.name, itemNumber: null, isFront: null  ]
-
-
-    // return (
-    //     <div>
-    //     {/* <h1>slider!</h1> */}
-    //     {/* <img src={images[0].path} alt="user swiper created" className="swiperImage"/> */}
-    //     <Swiper
-    //     modules={[Navigation, EffectCards, EffectFlip]}
-    //     // navigation
-    //     mousewheel= {true}
-    //     speed= {800}
-    //     slidesPerView={1}
-    //     effect= {'cards'}
-    //     className="mainSwiper"
-    //     >   <SwiperSlide className="swiperSlide">
-    //             <div class="card js-card">
-    //             <div class="card__wrapper">
-    //                 <div class="card__side is-active">
-    //                 <h4>FRONT</h4>
-    //                 <img src={images[0].path} alt="user swiper created" className="swiperImage"/>
-    //                 </div>
-    //                 <div class="card__side card__side--back">
-    //                 <img src={images[1].path} alt="user swiper created" className="swiperImage"/>
-    //                 <h2>BACK</h2>
-    //                 </div>
-    //             </div>
-    //             </div>
-
-    //             {/* <div className='flip-wrapper'
-    //             modules={[Navigation, EffectCards, EffectFlip]}
-    //             // navigation
-    //             mousewheel= {true}
-    //             speed= {800}
-    //             slidesPerView={1}
-    //             effect= {'cards'}
-    //             >
-    //                 <div className='FRONT'>
-    //                     <img src={images[0].path} alt="user swiper created" className="swiperImage"/>
-    //                 </div>
-    //                 <div className='BACK'>
-    //                     <img src={images[1].path} alt="user swiper created" className="swiperImage"/>
-    //                 </div>
-    //             </div> */}
-    //         </SwiperSlide>
-    //         <SwiperSlide className="swiperSlide">
-    //             <img src={images[2].path} alt="user swiper created" className="swiperImage"/>
-    //         </SwiperSlide>
-    //     </Swiper>
-    //     </div>
-    // )
